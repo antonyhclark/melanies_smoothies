5e347e12768ceb95df7c8a8ee44059b8484835c4
Dr Antony H. Clark <77163158+antonyhclark@users.noreply.github.com>
2025-01-15 17:20:44 +0000

    when_matched

--- streamlit_app_process.py
+++ streamlit_app_process.py
@@ -2,7 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 
-from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import col, when_matched
 # Write directly to the app
 st.title("Pending Smoothie Orders :frog:")
 st.write(
