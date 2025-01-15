fb73a66459662ae8f3fd979a46a00eff16a6d551
Dr Antony H. Clark <77163158+antonyhclark@users.noreply.github.com>
2025-01-15 15:48:46 +0000

    Update streamlit_app.py
    cosmetic change to test github streamlit interaction

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 # Write directly to the app
-st.title("Smoothie customisation :heart:")
+st.title("Smoothie customisation :heart: :frog:")
 st.write(
     """Choose!
     """
