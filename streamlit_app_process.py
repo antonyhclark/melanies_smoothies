a1ddc8632e6b000648ee923e321025300745300b
Dr Antony H. Clark <77163158+antonyhclark@users.noreply.github.com>
2025-01-15 16:34:05 +0000

    Update streamlit_app_process.py

--- streamlit_app_process.py
+++ streamlit_app_process.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
+
 from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title("Pending Smoothie Orders :frog:")
@@ -12,7 +13,9 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 
-session = get_active_session()
+#session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.orders").filter(
     col("ORDER_FILLED")==0).collect()
 #st.dataframe(my_dataframe)
