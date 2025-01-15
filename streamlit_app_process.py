3a14e62c136617437fc6bd2f6dfdf20964e4eb1c
Dr Antony H. Clark <77163158+antonyhclark@users.noreply.github.com>
2025-01-15 16:30:02 +0000

    Create streamlit_app_process.py

---
+++ streamlit_app_process.py
@@ -0,0 +1,23 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+# Write directly to the app
+st.title("Pending Smoothie Orders :frog:")
+st.write(
+    """Choose!
+    """
+)
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders").filter(
+    col("ORDER_FILLED")==0).collect()
+#st.dataframe(my_dataframe)
+editable_df = st.data_editor(my_dataframe)
+submitted = st.button('Submit')
+
+if submitted:
+    st.success('Some clicked the button.', icon = '✌️')
